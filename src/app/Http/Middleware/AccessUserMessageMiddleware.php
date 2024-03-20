0fbf4620ef45b441186f2c6abe1d7b2cc44a9517
VuacheslavMironov <vuacheslav.mir@gmail.com>
2024-03-20 21:03:23 +0700

    Промежуточный коммит: сделал посредники для обработки запросов к конкретным сообщениям.

---
+++ src/app/Http/Middleware/AccessUserMessageMiddleware.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Services\AccessMessageService;
+use App\Http\Services\MessageService;
+use App\Http\Services\UserService;
+use App\Models\AccessUserToMessage;
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AccessUserMessageMiddleware
+{
+    private UserService $_userService;
+    private AccessMessageService $_accessMessageService;
+    private MessageService $_messageService;
+    public function __construct(
+        UserService $userService,
+        AccessMessageService $accessMessageService,
+        MessageService $messageService
+    )
+    {
+        $this->_userService = $userService;
+        $this->_accessMessageService = $accessMessageService;
+        $this->_messageService = $messageService;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($this->_messageService->show($request['id'])->rule == 'concrete_visible' && !is_null(
+            $this->_accessMessageService->exists(
+                $request->id,
+                // Получение User::id по токену
+                $this->_userService->show(User::getUserByToken(
+                        explode(' ', $request->headers->get('authorization'))[2]
+                )->tokenable_id)->id
+            )
+        ))
+        {
+            return $next($request);
+        }
+        throw new \Exception('У вас нет доступа к сообщению!');
+    }
+}
